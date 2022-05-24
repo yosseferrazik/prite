104f936b9833e6f411f392d4bd1bca6f79813cc5
KhanessKA <82938832+KhanessKA@users.noreply.github.com>
2022-05-24 09:36:18 +0000

    xd

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = {
 	name: 'ready',
 	async execute(client, message) {
         
-    let activities = [`${config.prefix}help | @Prite `, "Hi \(o_o)/", ` ${config.prefix}help ⊙_⊙ `, "si te portas bien"];
+    let activities = [`${config.prefix}help | @Prite `, "tus calificaciones \(o_o)/", ` ${config.prefix}help \ @Prite`, "si te portas bien"];
     
     
     setInterval(() => {
